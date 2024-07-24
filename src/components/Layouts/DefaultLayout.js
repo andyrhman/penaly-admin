14b9f66f3247cbdae455ff636d678d9d7d717e88
Andy <andyrhman@andyrahman989@gmail.com>
2024-07-24 20:08:49 +0800

    Added login server integration

--- src/components/Layouts/DefaultLayout.js
+++ src/components/Layouts/DefaultLayout.js
@@ -1,10 +1,49 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Sidebar from "../Sidebar";
 import Header from "../Header";
+import axios from "axios";
+import Loader from "../common/Loader";
+import { useRouter } from "next/router";
+import { connect } from "react-redux";
+import { setUser } from "../../redux/actions/setUserAction";
 
-export default function DefaultLayout({ children }) {
+const DefaultLayout = (props) => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const { setUser } = props;
+  const router = useRouter();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await axios.get('user');
+
+        if (data.role_id !== 1 && data.role_id !== 2) {
+          // Redirect if the user is not an admin
+          router.push('/login');
+        } else {
+          // Set user data if the user is an admin
+          props.setUser({
+            id: data.id,
+            username: data.username,
+            email: data.email,
+            role: data.role,
+          });
+        }
+      } catch (error) {
+        if (error.response && [401, 403, 404].includes(error.response.status)) {
+          router.push('/login');
+        }
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, [setUser, router]);
+
+  if (loading) {
+    return <Loader />; // Replace with your loading spinner or component
+  }
   return (
     <>
       {/* <!-- ===== Page Wrapper Start ===== --> */}
@@ -22,7 +61,7 @@ export default function DefaultLayout({ children }) {
           {/* <!-- ===== Main Content Start ===== --> */}
           <main>
             <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
-              {children}
+              {props.children}
             </div>
           </main>
           {/* <!-- ===== Main Content End ===== --> */}
@@ -33,3 +72,18 @@ export default function DefaultLayout({ children }) {
     </>
   );
 }
+
+const mapStateToProps = (state) => {
+  // console.log(state.user); // * Always console log first for showing the response data
+  return {
+    user: state.user.user
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setUser: (user) => dispatch(setUser(user))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(DefaultLayout);
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,27 @@
 import Layout from '../components/Layout';
+import axios from 'axios';
 import { ToastContainer } from 'react-toastify';
+import { configStore } from '../redux/configureStore';
+import { Provider } from 'react-redux';
 import 'react-toastify/dist/ReactToastify.css';
 import "flatpickr/dist/flatpickr.min.css";
 import "../styles/globals.css";
 import "../styles/satoshi.css";
 
+axios.defaults.baseURL = process.env.NEXT_PUBLIC_SERVER_ENDPOINT;
+axios.defaults.withCredentials = true;
+
+const store = configStore();
+
 function MyApp({ Component, pageProps }) {
 
   return (
     <Layout>
-      <ToastContainer />
-      <Component {...pageProps} />
+      <Provider store={store}>
+
+        <ToastContainer />
+        <Component {...pageProps} />
+      </Provider>
     </Layout>
   );
 }
